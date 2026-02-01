29db1a060c8635c5f55c57f2c244a9d2f59e634c
chewingbytes <bryanchewzy24@gmail.com>
2026-02-01 15:32:13 +0800

    cors

--- server.js
+++ server.js
@@ -12,8 +12,26 @@ const PORT = process.env.PORT || 5050;
 
 app.set("trust proxy", 1);
 
+const allowedOrigins = [
+  "https://fwab-again.vercel.app"
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  }),
+);
+
 app.use(express.json());
-app.use(cors());
 app.use(cookieParser());
 app.use("/api/events", eventsRouter);
 app.use("/api/users", userRouter);
